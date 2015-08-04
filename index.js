09a4c5fb4e841dc2d5e2eb3ec198fbf6086173f1
kkdashu <wmeng17@gmail.com>
2015-08-04 16:39:04 +0800

    增加bootstrap.css的依赖

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ module.exports = {
   included: function(app) {
     this._super.included(app);
 
-    //app.import(app.bowerDirectory + '/moment/moment.js');
+    app.import(app.bowerDirectory + '/bootstrap/dist/css/bootstrap.css');
     //app.import(app.bowerDirectory + '/pikaday/pikaday.js');
     //app.import(app.bowerDirectory + '/pikaday/css/pikaday.css');
   }
