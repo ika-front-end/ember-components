53c615b3472e1becd069affb7e1b733e170ac5d3
kkdashu <wmeng17@gmail.com>
2015-08-05 23:51:42 +0800

    增加header的css文件

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ module.exports = {
     this._super.included(app);
 
     app.import(app.bowerDirectory + '/bootstrap/dist/css/bootstrap.css');
-    //app.import(app.bowerDirectory + '/pikaday/pikaday.js');
-    //app.import(app.bowerDirectory + '/pikaday/css/pikaday.css');
+    app.import('vendor/styles/header.css');
   }
 };
