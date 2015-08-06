43ae31c446d0350af323d9ee474cf621bf1fd896
wangmeng <wmeng17@gmail.com>
2015-08-06 15:25:49 +0800

    增加normalize.css

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ module.exports = {
   included: function(app) {
     this._super.included(app);
 
-    //app.import(app.bowerDirectory + '/bootstrap/dist/css/bootstrap.css');
+    app.import(app.bowerDirectory + '/normalize.css/normalize.css');
     app.import('vendor/styles/header.css');
   }
 };
